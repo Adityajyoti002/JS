2b90a2c42c37148ef8445e5193a4658f4ec83984
Aditya Jyoti <110086332+Adityajyoti002@users.noreply.github.com>
2023-07-31 14:36:09 +0200

    type 03_ConversionOperations

---
+++ 03_ConversionOperations.js
@@ -0,0 +1,29 @@
+let score = "aditya"
+
+console.log(typeof score)
+console.log(typeof(score))
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+
+//"33" => 33
+//"33abc" => NaN
+// true =>1; false => 0
+
+
+let IsLoggedIn = "aditya"
+
+let booleanIsLoggedIn = Boolean(IsLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+//"" => false
+// "aditya" => true
+
+let somenumber = 33
+let stringsomenumber = String(somenumber)
+
+console.log(stringsomenumber);
+console.log(typeof stringsomenumber);
